8634c84663fd9177866393de72bfdb4eb239c0ba
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-06-28 18:36:39 -0300

    integrando upload com o cloudflare

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -1,36 +1,43 @@
-import { db } from '@/infra/db'
-import { schema } from '@/infra/db/schemas'
-import { type Either, makeLeft, makeRight } from '@/shared/either'
-import { Readable } from 'node:stream'
-import { z } from 'zod'
-import { InvalidFileFormat } from './errors/invalid-file-format'
+import { db } from "@/infra/db";
+import { schema } from "@/infra/db/schemas";
+import { uploadFileToStorage } from "@/infra/storage/upload-file-to-storage";
+import { type Either, makeLeft, makeRight } from "@/shared/either";
+import { Readable } from "node:stream";
+import { z } from "zod";
+import { InvalidFileFormat } from "./errors/invalid-file-format";
 
 const uploadImageInput = z.object({
   fileName: z.string(),
   contentType: z.string(),
   contentStream: z.instanceof(Readable),
-})
+});
 
-type UploadImageInput = z.input<typeof uploadImageInput>
+type UploadImageInput = z.input<typeof uploadImageInput>;
 
-const allowedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
+const allowedMimeTypes = ["image/jpg", "image/jpeg", "image/png", "image/webp"];
 
 export async function uploadImage(
   input: UploadImageInput
 ): Promise<Either<InvalidFileFormat, { url: string }>> {
-  const { contentStream, contentType, fileName } = uploadImageInput.parse(input)
+  const { contentStream, contentType, fileName } =
+    uploadImageInput.parse(input);
 
   if (!allowedMimeTypes.includes(contentType)) {
-    return makeLeft(new InvalidFileFormat())
+    return makeLeft(new InvalidFileFormat());
   }
 
-  // TODO: carregar a imagem p/ o Cloudflare R2
+  const { key, url } = await uploadFileToStorage({
+    folder: "images",
+    fileName,
+    contentType,
+    contentStream,
+  });
 
   await db.insert(schema.uploads).values({
     name: fileName,
-    remoteKey: fileName,
-    remoteUrl: fileName,
-  })
+    remoteKey: key,
+    remoteUrl: url,
+  });
 
-  return makeRight({ url: '' })
+  return makeRight({ url });
 }
--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,21 +1,21 @@
-import { uploadImage } from '@/app/functions/upload-image'
-import { isRight, unwrapEither } from '@/shared/either'
-import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-import { z } from 'zod'
+import { uploadImage } from "@/app/functions/upload-image";
+import { isRight, unwrapEither } from "@/shared/either";
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod";
 
-export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
+export const uploadImageRoute: FastifyPluginAsyncZod = async (server) => {
   server.post(
-    '/uploads',
+    "/uploads",
     {
       schema: {
-        summary: 'Upload an image',
-        consumes: ['multipart/form-data'],
+        summary: "Upload an image",
+        consumes: ["multipart/form-data"],
         response: {
-          201: z.null().describe('Image uploaded'),
+          201: z.null().describe("Image uploaded"),
           400: z.object({ message: z.string() }),
           409: z
             .object({ message: z.string() })
-            .describe('Upload already exist.'),
+            .describe("Upload already exist."),
         },
       },
     },
@@ -24,28 +24,29 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         limits: {
           fileSize: 1024 * 1024 * 2, // 2mb
         },
-      })
+      });
 
       if (!uploadedFile) {
-        return reply.status(400).send({ message: 'File is required.' })
+        return reply.status(400).send({ message: "File is required." });
       }
 
       const result = await uploadImage({
         fileName: uploadedFile.filename,
         contentType: uploadedFile.mimetype,
         contentStream: uploadedFile.file,
-      })
+      });
 
       if (isRight(result)) {
-        return reply.status(201).send()
+        console.log(unwrapEither(result));
+        return reply.status(201).send();
       }
 
-      const error = unwrapEither(result)
+      const error = unwrapEither(result);
 
       switch (error.constructor.name) {
-        case 'InvalidFileFormat':
-          return reply.status(400).send({ message: error.message })
+        case "InvalidFileFormat":
+          return reply.status(400).send({ message: error.message });
       }
     }
-  )
-}
+  );
+};
